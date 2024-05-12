85d967e6a6f927d9b6729a980f390eca23135425
Arpit7986 <102222929+Arpit7986@users.noreply.github.com>
2024-05-12 19:05:08 +0530

    Update googleauthentication.js

--- routes/googleauthentication.js
+++ routes/googleauthentication.js
@@ -39,10 +39,14 @@ passport.authenticate('google', {
 })
 );
 
-router.get('/auth/protected', async (req, res) => {
- 
-    res.redirect('/smartmenu');
-
+router.get('/auth/protected', isLoggedIn,async (req, res) => {
+    let name = req.user.displayName;
+    let email=req.user.email
+    let id=req.user.id
+    let existingUser=await Google.findOne({email})
+    if(!existingUser)
+    await Google.create({name,email})   
+    res.redirect('/smartmenu')
 });
 router.get('/auth/google/failure', (req, res) => {
     res.send('Failure');
