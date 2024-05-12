8b6dd277aa2b85e6551ab4bec59fa2d65882cfb9
Arpit7986 <102222929+Arpit7986@users.noreply.github.com>
2024-05-12 17:38:24 +0530

    Update googleauthentication.js

--- routes/googleauthentication.js
+++ routes/googleauthentication.js
@@ -9,7 +9,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/auth/google/callback",
+    callbackURL: "https://smart-restaurant-menu.onrender.com/auth/google/callback",
     passReqToCallback: true
 },
 function(request, accessToken, refreshToken, profile, done) {
